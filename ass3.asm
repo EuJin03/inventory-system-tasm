15ac52c2a3d9ea1d2b2c8c9d3dce3955a03bf52d
eugene <eugenetin99@gmail.com>
2023-04-29 08:56:08 +0800

    -update: complete program

--- ass3.asm
+++ ass3.asm
@@ -11,7 +11,9 @@
 	inventory_name_offset dw 20
 	inventory_quantity_offset dw 120
 	inventory_price_offset dw 140
-
+	sales dw 0,0,0,0,0,0,0,0,0,0,'$' ; quantity sold & total price
+	item_price dw 1, 7, 1, 1, 2, 7, 1, 2, 7, 1, '$'
+	
 	; syntax formatting
 	crlf db 13,10,'$'
 	sline db 13,10, '----------------------------------------------' ,'$'
@@ -19,13 +21,13 @@
 	dotted db '**********************************************','$'
 
 	; main menu
-	menu db 13,10, '----------<APU MINI GROCERY STALL>----------',13,10, '------------------MAIN MENU-----------------', 13, 10, 10 ,'1. View Inventory',13,10,'2. Restock Item',13,10,'3. Sell Items',13,10, '4. Search Item',13,10,'5. Sales Report',13,10,'0. Exit',13,10,'$'
+	menu db 13, '----------<APU MINI GROCERY STALL>----------',13,10, '------------------MAIN MENU-----------------', 13, 10, 10 ,'1. View Inventory',13,10,'2. Restock Item',13,10,'3. Sell Items',13,10, '4. Sort Items',13,10,'5. Sales Report',13,10,'0. Exit',13,10,'$'
 	invalid_input db 13,10,'Invalid input. Please try again.',13,10,'$'
 	
 	; CRUD labels
 	; view inventory
 	inventory_header db 13,10, '----------<APU MINI GROCERY STALL>----------',13,10, '----------------<INVENTORY>-----------------',13,10,'ID',9,'Name',9,9, 'Quantity',9, 'Price',13,10,'$'
-	inventory_label db '==============================================', 13, 10, 'Items that needs to be restock are displayed as RED!', 13, 10, '==============================================', 13, 10, '1. Go Back', 13, 10, '0. Exit the Program', 13, 10 , 13, 10,'Enter your choice: $'
+	inventory_label db '==============================================', 13, 10, 'Items that needs to be restock are displayed as RED!', 13, 10, '==============================================', 13, 10, '1. Back to Main Menu', 13, 10,  '2. Restock Items', 13, 10, '3. Sell Items', 13, 10 , 13, 10,'Enter your choice: $'
 	
 	stock_amount dw ?
 	stock_id dw ?
@@ -35,10 +37,19 @@
 	restock_amount_label db 13, 10, 'Enter the amount to restock (between 1-9): $'
 	restock_success db 13, 10, 'Item has been restocked successfully!', 13, 10, '$'
 	; sell items 
-	sell_item_id_label db 13, 10, 'Enter the item ID to sell: $'
+	sell_item_id_label db '==============================================', 13, 10,9,9, 32,32,'SELL ITEM', 13, 10, '==============================================', 13, 10,  'Enter the item ID to sell: $'
 	sell_item_amount_label db 13, 10, 'Enter the amount to sell (between 1-9): $'
 	sell_item_success db 13, 10, 'Item has been sold successfully!', 13, 10, '$'
 	sell_item_fail db 13, 10, 'Item cannot be sold, not enough quantity!', 13, 10, '$'
+
+	;categorize inventory
+	sort_inventory_label db 13, '==============================================', 13, 10,9,'SORT INVENTORY BY STOCK COUNT', 13, 10, '==============================================', 13, 10, '1. Back to Main Menu', 13, 10, '2. In Stock', 13, 10, '3. Low/Out Of Stock', 13, 10, 13, 10, 'Enter your choice: $'
+	low_stock_label db 13, 10, 'Items are Low On Stock!', 13, 10, '$'
+	no_stock_label db 13, 10, 'Items are Out Of Stock!', 13, 10, '$'
+	on_stock_label db 13, 10, 'Items are In Stock!', 13, 10, '$'
+	
+	;sales made
+
 
 	; misc
 	user_choice db 13, 10, 'Enter your choice: $'
@@ -67,7 +78,7 @@ main proc
 	je sales_inventory_interface
 
 	cmp al, '4'
-	je search_inventory_interface
+	je sort_inventory_interface
 	
 	cmp al, '5'
 	je sales_report_interface
@@ -94,11 +105,13 @@ main proc
 		call view_inventory
 		call sales_inventory
 		ret
-	search_inventory_interface:
-		call search_inventory
+	sort_inventory_interface:
+		call sort_inventory
+		call user_navigate
 		ret
 	sales_report_interface:
 		call sales_report
+		call user_navigate
 		ret
 	exit_program_interface:
 		call clear_screen
@@ -122,6 +135,12 @@ user_navigate:
 
 	cmp al, '1'
 	je main
+
+	cmp al, '2'
+	je restock_inventory_interface
+	
+	cmp al, '3'
+	je sales_inventory_interface
 	
 	jmp main
 	ret
@@ -226,19 +245,19 @@ view_inventory:
 		mov ax, [si] ; load inventory id into ax
 		cmp ax, 10 ; check if end of array
 		ja done 
-		call print_int
+		call print_int ; print the integer
 
 		call print_tab
 
-		mov dx, offset inventory + 20
-		add dx, bp
-		call print_string 
-		mov ax, [si + 120]
-		call check_int
+		mov dx, offset inventory + 20 ; load inventory name into dx
+		add dx, bp ; add bp to dx to point to the next word
+		call print_string ; print the string
+		mov ax, [si + 120] ; load inventory stock into ax
+		call check_int ; check if stock is less than or equal to 5
 
 		call print_tab
 		
-		mov ax, [si + 120]
+		mov ax, [si + 120] 
 		call print_int
 
 		call print_double_tab
@@ -280,7 +299,7 @@ restock_inventory:
 	lea si, inventory
 	add si, stock_id
 	add cx, [si]
-	mov word ptr [si], cx
+	mov word ptr [si], cx 
 	
 	call clear_screen
 	call print_newline
@@ -303,9 +322,9 @@ sales_inventory:
 	mov ah, 01
 	int 21h
 
-	sub al, 30h 
-	add al, al
-	sub ax, 136
+	sub al, 30h ; convert to integer
+	add al, al ; multiply by 2
+	sub ax, 136 ; subtract 136 to get the offset
 	mov stock_id, ax 
 
 	lea dx, sell_item_amount_label
@@ -320,7 +339,7 @@ sales_inventory:
 
 	lea si, inventory
 	add si, stock_id
-	mov bx, [si] 
+	mov bx, [si] ; load stock into bx
 	sub bx, cx
 	cmp bx, 0
 	js reset_quantity
@@ -341,9 +360,10 @@ sales_inventory:
 		call print_newline
 		call view_inventory
 		call user_navigate
-		ret
+		ret 
 	
 	sold_quantity:
+	  call sales_done
 		call clear_screen
 		call print_newline
 		call print_asterisk
@@ -355,16 +375,187 @@ sales_inventory:
 		call view_inventory
 		call user_navigate
 		ret
+	sales_done: 
+		mov ax, stock_id 
+		sub ax, 120 
+		mov stock_id, ax
+		lea si, sales 
+		add si, stock_id
+		mov ax, [si]
+		add cx, ax 
+		mov word ptr [si], cx
+		ret
 	ret
 
-search_inventory:
+sort_inventory:
 	; Code to search item
-	call view_inventory
+	call clear_screen
+	lea dx, sort_inventory_label
+	mov ah, 09h 
+	int 21h 
+	
+	mov ah, 01h 
+	int 21h 
+	 
+	cmp al, '2'
+	je in_stock_prompt
+	
+	cmp al, '3' 
+	je low_in_stock_prompt 
+	
+	cmp al, '1'
+	call sort_inventory
+
 	ret
+
+in_stock_prompt:
+	call clear_screen
+	mov dx, offset inventory_header
+	mov ah, 09
+	int 21h
+	
+	mov bp, 0
+	lea si, inventory
+
+	loop_start2:
+	  mov ax, [si] 
+		cmp ax, 10 
+		ja end2
+
+		mov ax, [si + 120] ; load inventory id into ax
+		cmp ax, 6 ; check if end of array
+		jl done2 
+
+		mov ax, [si]
+		call print_int ; print the integer
+
+		call print_tab
+
+		mov dx, offset inventory + 20 ; load inventory name into dx
+		add dx, bp ; add bp to dx to point to the next word
+		call print_string ; print the string
+		mov ax, [si + 120] ; load inventory stock into ax
+		call check_int ; check if stock is less than or equal to 5
+
+		call print_tab
+		
+		mov ax, [si + 120] 
+		call print_int
+
+		call print_double_tab
+		
+		mov ax, [si + 140]
+		call print_int
+		call print_newline
+
+		add bp, 10
+		add si, 2 ; increment SI to point to the next word
+		jmp loop_start2 ; repeat the loop for the next element
+	done2:
+	  add bp, 10
+		add si, 2
+		jmp loop_start2
+		ret 
+	end2:
+	  ret
+	ret
+
+low_in_stock_prompt:
+	call clear_screen
+	mov dx, offset inventory_header
+	mov ah, 09
+	int 21h
+	
+	mov bp, 0
+	lea si, inventory
+
+	loop_start3:
+	  mov ax, [si] 
+		cmp ax, 10 
+		ja end3
+
+		mov ax, [si + 120] ; load inventory id into ax
+		cmp ax, 5 ; check if end of array
+		jg done3
+
+		mov ax, [si]
+		call print_int ; print the integer
+
+		call print_tab
+
+		mov dx, offset inventory + 20 ; load inventory name into dx
+		add dx, bp ; add bp to dx to point to the next word
+		call print_string ; print the string
+		mov ax, [si + 120] ; load inventory stock into ax
+		call check_int ; check if stock is less than or equal to 5
+
+		call print_tab
+		
+		mov ax, [si + 120] 
+		call print_int
+
+		call print_double_tab
+		
+		mov ax, [si + 140]
+		call print_int
+		call print_newline
+
+		add bp, 10
+		add si, 2 ; increment SI to point to the next word
+		jmp loop_start3 ; repeat the loop for the next element
+	done3:
+	  add bp, 10
+		add si, 2
+		jmp loop_start3
+		ret 
+	end3:
+	  ret
+  ret 
 
 sales_report:
-	; Code to generate sales report
-	call view_inventory
+  call clear_screen
+	; Code to generate sales reports
+	mov dx, offset inventory_header
+	mov ah, 09
+	int 21h
+
+  mov bp, 0
+	lea si, inventory
+	mov bx, offset sales 
+	mov di, offset item_price
+
+	loop_start5:
+		mov ax, [si] ; load inventory id into ax
+		cmp ax, 10 ; check if end of array
+		ja done5 
+		call print_int ; print the integer
+
+		call print_tab
+
+		mov dx, offset inventory + 20 ; load inventory name into dx
+		add dx, bp ; add bp to dx to point to the next word
+		call print_string ; print the string
+
+		call print_tab
+		
+		mov ax, [bx]
+		call print_int
+
+		call print_double_tab
+
+		mov cx, [bx]
+		mov ax, [di]
+		mul cx
+		call print_int
+	
+		call print_newline
+
+    add bp, 10
+		add si, 2 ; increment SI to point to the next word
+		add bx, 2
+		add di, 2
+		jmp loop_start5 ; repeat the loop for the next element
+	done5:
 	ret
 
 ; **********************************************
@@ -399,8 +590,6 @@ draw_line:
 	mov ah, 09h
 	int 21h
 	ret
-
-
 
 print_tab:
 	mov dl, 09
